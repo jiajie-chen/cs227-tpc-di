0a175122c4df77ba32b8477e43e09532547e043a
Novopia <tomyao0@gmail.com>
2016-04-15 14:41:22 -0400

    Connect to the socket and read

--- src/main/java/TPCDIClient.java
+++ src/main/java/TPCDIClient.java
@@ -1,5 +1,33 @@
 /**
  * Created by Lexi on 3/16/16.
  */
+import java.io.BufferedInputStream;
+import java.net.Socket;
+
 public class TPCDIClient {
+
+    public static void main(String[] args) {
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket(TPCDIConstants.STREAMINGESTOR_HOST, TPCDIConstants.STREAMINGESTOR_PORT);
+            clientSocket.setSoTimeout(5000);
+
+            BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
+
+            while (true) {
+                int length = in.read();
+                if (length == -1) break;
+                byte[] messageByte = new byte[length];
+                in.read(messageByte);
+                String tuple = new String(messageByte);
+                String[] curTuple = new String[1];
+                curTuple[0] = tuple;
+                System.out.println(tuple);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/TPCDIConstants.java
+++ src/main/java/TPCDIConstants.java
@@ -2,4 +2,6 @@
  * Created by Lexi on 3/16/16.
  */
 public class TPCDIConstants {
+    public static final int STREAMINGESTOR_PORT = 18000;
+    public static final String STREAMINGESTOR_HOST = "localhost";
 }
