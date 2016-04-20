2066dbffd0066b476823302a26cc491416cf8e00
Novopia <tomyao0@gmail.com>
2016-04-20 16:26:11 -0400

    Fix the bug of closing the connection

--- src/main/java/TPCDIClient.java
+++ src/main/java/TPCDIClient.java
@@ -57,7 +57,6 @@ public class TPCDIClient {
                     " part_id int NOT NULL" +
                     ")";
             stmt.execute(sql);
-            c.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -74,39 +73,39 @@ public class TPCDIClient {
         } catch (Exception e) {
             e.printStackTrace();
         }
-            while (true) {
-                int length = 0;
-                try {
-                    length = in.read();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if (length == -1) break;
-                byte[] messageByte = new byte[length];
-                try {
-                    in.read(messageByte);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                String tuple = new String(messageByte);
-                if (tuple == "") break;
-
-                String[] st = tuple.split("\\|", -1);
-                long T_ID = new Long(st[0]);
-                try {
-                    Statement stmt = c.createStatement();
-                    String insertSQL = "INSERT INTO SP1out (T_ID, T_DTS, T_ST_ID, T_TT_ID, T_IS_CASH, T_S_SYMB" +
-                            ", T_QTY, T_BID_PRICE, T_CA_ID, T_EXEC_NAME, T_TRADE_PRICE, T_CHRG, T_COMM, T_TAX, batch_id, part_id) VALUES (" +
-                            T_ID + ", \'" + st[1] + "\', \'" + st[2] + "\', \'" + st[3] + "\', " + new Short(st[4]) + ", \'" +
-                            st[5] + "\', " + new Integer(st[6]) + ", " + new Double(st[7]) + ", " + new Integer(st[8]) + ", " +
-                            st[9] + ", " + new Double(replaceWithZeroIfEmpty(st[10])) + ", " + new Double(replaceWithZeroIfEmpty(st[11])) + ", " +
-                            new Double((replaceWithZeroIfEmpty(st[12]))) + ", " + new Double(replaceWithZeroIfEmpty(st[13])) + ", " + batchid + ", " +
-                            partid + ");";
-                    stmt.execute(insertSQL);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        while (true) {
+            int length = 0;
+            try {
+                length = in.read();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            if (length == -1) break;
+            byte[] messageByte = new byte[length];
+            try {
+                in.read(messageByte);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            String tuple = new String(messageByte);
+            if (tuple == "") break;
+
+            String[] st = tuple.split("\\|", -1);
+            long T_ID = new Long(st[0]);
+            try {
+                Statement stmt = c.createStatement();
+                String insertSQL = "INSERT INTO SP1out (T_ID, T_DTS, T_ST_ID, T_TT_ID, T_IS_CASH, T_S_SYMB" +
+                        ", T_QTY, T_BID_PRICE, T_CA_ID, T_EXEC_NAME, T_TRADE_PRICE, T_CHRG, T_COMM, T_TAX, batch_id, part_id) VALUES (" +
+                        T_ID + ", \'" + st[1] + "\', \'" + st[2] + "\', \'" + st[3] + "\', " + new Short(st[4]) + ", \'" +
+                        st[5] + "\', " + new Integer(st[6]) + ", " + new Double(st[7]) + ", " + new Integer(st[8]) + ", " +
+                        st[9] + ", " + new Double(replaceWithZeroIfEmpty(st[10])) + ", " + new Double(replaceWithZeroIfEmpty(st[11])) + ", " +
+                        new Double((replaceWithZeroIfEmpty(st[12]))) + ", " + new Double(replaceWithZeroIfEmpty(st[13])) + ", " + batchid + ", " +
+                        partid + ");";
+                stmt.execute(insertSQL);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
 
     }
